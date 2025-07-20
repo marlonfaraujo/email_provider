89984c60b90ad7e73e477c369c0e80a6c41930ca
Marlon <marlonfda@gmail.com>
2025-07-20 14:43:11 -0300

    More requests in integration tests

--- http-test/src/http.test.ts
+++ http-test/src/http.test.ts
@@ -2,6 +2,10 @@ import axios from 'axios';
 
 test("Check endpoints", async () => {
     const baseUrl = process.env.BASE_URL || 'http://app-email-provider:8080';
-    const response = await axios.get(`${baseUrl}/api/metrics`);
-    expect(response.status).toBe(200);
+    const apiResponse = await axios.get(`${baseUrl}/api/metrics`);
+    expect(apiResponse.status).toBe(200);
+    const brokerPublisherResponse = await axios.get(`${baseUrl}/broker-publisher/custom-tracer`);
+    expect(brokerPublisherResponse.status).toBe(200);
+    const kafkaExporterResponse = await axios.get(`${baseUrl}/kafka-exporter/metrics`);
+    expect(kafkaExporterResponse.status).toBe(200);
 });
